ce28d7022987544fb88c5755cf98426a4f7f1361
abhishek acharya <85024420+abhishekacharya-hub@users.noreply.github.com>
2022-11-13 19:39:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+let x= 10;
+if( x === 10) {
+    let x = 20
+    console.log(x); //20
+}
+console.log(x) //10
+
+//arrow funtion
+
+//with function expression
+let add = function (x, y) {
+    return x + y;
+  };
+  console.log(add(12, 23));
+
+//with arrow funtion
+let add1 = (x, y) => {
+    return x + y;
+  };
+  console.log(add1(23, 34));
+
+
+//arrow function with no arg
+let greet = () => console.log("hello")
+greet();
+
+//arrow functin with one arg
+let greet1 = x => console.log(x)
+greet('hello');
+
+//with dynamically created function as an function expreesion
+let age = 26;
+let welcome =
+  age > 18
+    ? () => console.log("person is adult")
+    : () => console.log("person is child");
+welcome();
